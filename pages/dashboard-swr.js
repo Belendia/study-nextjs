1abf53eef57f5236146ab74bfaa1d0a02eb218cf
Belendia Serda <belendia@gmail.com>
2022-07-18 12:14:17 +0300

    Reimplement dashboard page by replacing data fetching logic with SWR hook.

---
+++ pages/dashboard-swr.js
@@ -0,0 +1,31 @@
+import useSWR from "swr";
+
+const fetcher = async () => {
+  const response = await fetch("http://localhost:4444/dashboard");
+  const data = await response.json();
+  return data;
+};
+
+function DashboardSWR() {
+  const { data, error } = useSWR("dashboard", fetcher);
+
+  if (error) {
+    return <h2>Error</h2>;
+  }
+
+  if (!data) {
+    return <h2>Loading ...</h2>;
+  }
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <h4>Post - {data.posts}</h4>
+      <h4>Likes - {data.likes}</h4>
+      <h4>Followers - {data.followers}</h4>
+      <h4>Following - {data.following}</h4>
+    </div>
+  );
+}
+
+export default DashboardSWR;
