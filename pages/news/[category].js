471bc7aca954d90640a434dea1c3b0f5028a3d07
Belendia Serda <belendia@gmail.com>
2022-07-18 11:35:09 +0300

    Distructure the context props to see what else we can find out of it

--- pages/news/[category].js
+++ pages/news/[category].js
@@ -21,8 +21,12 @@ function ArticleListByCategory({ articles, category }) {
 export default ArticleListByCategory;
 
 export async function getServerSideProps(context) {
-  const { params } = context;
+  const { params, req, res, query } = context;
   const { category } = params;
+
+  console.log(req.headers.cookie);
+  console.log(query);
+  res.setHeader("Set-Cookie", ["name=Belendia"]);
 
   const response = await fetch(
     `http://localhost:4444/news?category=${category}`
