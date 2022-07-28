ab7cbbc0b937f410fbbd1dc8233a8be822965300
Belendia Serda <belendia@gmail.com>
2022-07-28 22:02:53 +0300

    Add server side authentication

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,23 @@
-function Blog() {
-  return <h1>Blog Page</h1>;
+import { getSession } from "next-auth/react";
+
+function Blog({ data }) {
+  return <h1>Blog Page: {data}</h1>;
 }
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  // check if the user is logged in before fetching data
+
+  // Unlike calling getSession hook in the client side as we did in the _app.js
+  // when calling it in the server side we should pass a context.
+  const session = await getSession(context);
+
+  return {
+    props: {
+      data: session
+        ? "List of 100 personalized blog posts"
+        : "List of free blog posts",
+    },
+  };
+}
